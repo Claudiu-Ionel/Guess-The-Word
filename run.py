f4ee0a2c6808967b726b5891715d067e34d701ac
Claudiu Ionel <ionel.claudiu394@gmail.com>
2023-10-29 12:14:26 +0100

    added function that hides word guess

--- run.py
+++ run.py
@@ -1,8 +1,21 @@
 """Module providing a random number generator function"""
 import random
 
+#Reusable Functions
+def hide_word(word):
+    """Function that changes all characters in a word to "*"."""
+    hidden_word = word
+    for letter in hidden_word:
+        hidden_word = hidden_word.replace(letter, "*")
+    return hidden_word
+
+
+
 words_list = ["test", "longitude", "karma", "samurai",
               "language", "beginner", "alphabet", "envy", "binocular", "computation"]
-
-secret_word = words_list[random.randint(1, len(words_list) - 1)] #run (\n)
+secret_word = words_list[random.randint(1, len(words_list) - 1)] 
+word_guessed = hide_word(secret_word)
 print(secret_word)
+print(word_guessed) #run (\n)
+
+#End of file (EOF)
